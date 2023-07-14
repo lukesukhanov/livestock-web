4b1e20ee5599c722b4cd9813307c905700ae61e0
Luke Sukhanov <lukesukhanov@gmail.com>
2023-07-14 03:55:01 +0300

    Fix api url

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,15 +1,15 @@
 // User API
-export const USERS_API_URL = "http://127.0.0.1:9000/users";
-export const LOGIN_ON_LIVESTOCK_API_URL = "http://127.0.0.1:9000/login";
-export const AUTHORIZE_ON_LIVESTOCK_API_URL = "http://127.0.0.1:9000/oauth2/authorize";
-export const TOKEN_FROM_LIVESTOCK_API_URL = "http://127.0.0.1:9000/oauth2/token";
+export const USERS_API_URL = "http://localhost:9000/users";
+export const LOGIN_ON_LIVESTOCK_API_URL = "http://localhost:9000/login";
+export const AUTHORIZE_ON_LIVESTOCK_API_URL = "http://localhost:9000/oauth2/authorize";
+export const TOKEN_FROM_LIVESTOCK_API_URL = "http://localhost:9000/oauth2/token";
 export const CLIENT_ID_FOR_LIVESTOCK = "livestock-web";
-export const CLIENT_REDIRECT_URL_FOR_LIVESTOCK = "http://127.0.0.1:5500";
+export const CLIENT_REDIRECT_URL_FOR_LIVESTOCK = "http://localhost:5500";
 
 // Product API
-export const PRODUCT_CATEGORY_API_URL = "http://127.0.0.1:8082/categories";
-export const PRODUCT_API_URL = "http://127.0.0.1:8082/products";
-export const PRODUCT_IMAGES_API_URL = "http://127.0.0.1:8082/productImages";
+export const PRODUCT_CATEGORY_API_URL = "http://localhost:8082/categories";
+export const PRODUCT_API_URL = "http://localhost:8082/products";
+export const PRODUCT_IMAGES_API_URL = "http://localhost:8082/productImages";
 
 // Cart API
-export const CART_API_URL = "http://127.0.0.1:8083/productsInCart";
+export const CART_API_URL = "http://localhost:8083/productsInCart";
