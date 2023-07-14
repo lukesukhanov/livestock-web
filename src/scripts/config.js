fc2c363578418078d2cb3b0bf058070531aabe52
Luke Sukhanov <lukesukhanov@gmail.com>
2023-07-14 18:25:07 +0300

    Add config for stage profile

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,15 +1,31 @@
-// User API
-export const USERS_API_URL = "http://localhost:9000/users";
-export const LOGIN_ON_LIVESTOCK_API_URL = "http://localhost:9000/login";
-export const AUTHORIZE_ON_LIVESTOCK_API_URL = "http://localhost:9000/oauth2/authorize";
-export const TOKEN_FROM_LIVESTOCK_API_URL = "http://localhost:9000/oauth2/token";
+// // User API for dev profile
+// export const USERS_API_URL = "http://localhost:9000/users";
+// export const LOGIN_ON_LIVESTOCK_API_URL = "http://localhost:9000/login";
+// export const AUTHORIZE_ON_LIVESTOCK_API_URL = "http://localhost:9000/oauth2/authorize";
+// export const TOKEN_FROM_LIVESTOCK_API_URL = "http://localhost:9000/oauth2/token";
+// export const CLIENT_ID_FOR_LIVESTOCK = "livestock-web";
+// export const CLIENT_REDIRECT_URL_FOR_LIVESTOCK = "http://localhost:5500";
+
+// // Product API for dev profile
+// export const PRODUCT_CATEGORY_API_URL = "http://localhost:8082/categories";
+// export const PRODUCT_API_URL = "http://localhost:8082/products";
+// export const PRODUCT_IMAGES_API_URL = "http://localhost:8082/productImages";
+
+// // Cart API for dev profile
+// export const CART_API_URL = "http://localhost:8083/productsInCart";
+
+// User API for stage profile
+export const USERS_API_URL = "http://localhost:8765/users";
+export const LOGIN_ON_LIVESTOCK_API_URL = "http://localhost:8765/login";
+export const AUTHORIZE_ON_LIVESTOCK_API_URL = "http://localhost:8765/oauth2/authorize";
+export const TOKEN_FROM_LIVESTOCK_API_URL = "http://localhost:8765/oauth2/token";
 export const CLIENT_ID_FOR_LIVESTOCK = "livestock-web";
 export const CLIENT_REDIRECT_URL_FOR_LIVESTOCK = "http://localhost:5500";
 
-// Product API
-export const PRODUCT_CATEGORY_API_URL = "http://localhost:8082/categories";
-export const PRODUCT_API_URL = "http://localhost:8082/products";
-export const PRODUCT_IMAGES_API_URL = "http://localhost:8082/productImages";
+// Product API for stage profile
+export const PRODUCT_CATEGORY_API_URL = "http://localhost:8765/categories";
+export const PRODUCT_API_URL = "http://localhost:8765/products";
+export const PRODUCT_IMAGES_API_URL = "http://localhost:8765/productImages";
 
-// Cart API
-export const CART_API_URL = "http://localhost:8083/productsInCart";
+// Cart API for stage profile
+export const CART_API_URL = "http://localhost:8765/productsInCart";
